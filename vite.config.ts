0a0c1ce950de57248aa87d12997867c6dcbb1063
Olusegun Omilabu <omilabuolusegun@gmail.com>
2024-10-21 17:24:38 +0100

    test middleware

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
+const myPlugin = {
+	name: 'log-request-middleware',
+	configureServer(server: { middlewares: { use: (arg0: (req: any, res: any, next: any) => void) => void; }; }) {
+		server.middlewares.use((req, res, next) => {
+			console.log(`Got request ${req.url}`);
+			next();
+		});
+	}
+};
+
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [myPlugin, sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
